c69f40ac4cc0b532344af2723b57e123e0d303d1
Casey Lee <dawnrice6299@gmail.com>
2020-06-04 23:02:45 +0900

    ex2-11 done

--- DataStructure/ex2-11.java
+++ DataStructure/ex2-11.java
@@ -28,9 +28,8 @@ class test{
 			YMD temp = new YMD(this.y, this.m, this.d);
 			temp.d += n;
 			while(temp.d > months[isLeap(temp.y)][temp.m-1]) {
-				temp.m++;
 				temp.d -= months[isLeap(temp.y)][temp.m-1];
-				if(temp.m > 12) {
+				if(++temp.m > 12) {
 					temp.y++;
 					temp.m = 1;
 				}
@@ -38,33 +37,38 @@ class test{
 			return temp;
 		}
 
-//		YMD after(int n) {
-//			YMD temp = new YMD(this.y, this.m, this.d);
-//			temp.d += n;
-//
-//			while (temp.d > months[isLeap(temp.y)][temp.m - 1]) {
-//				temp.d -= months[isLeap(temp.y)][temp.m - 1];
-//				if (++temp.m > 12) {
-//					temp.y++;
-//					temp.m = 1;
-//				}
-//			}
-//			return temp;
-//		}
+		YMD before(int n) {
+			YMD temp = new YMD(this.y, this.m, this.d);
+			temp.d -= n;
+			while(temp.d<1) {
+				if(--temp.m < 1) {
+					temp.y--;
+					temp.m = 12;
+				}
+				temp.d += months[isLeap(temp.y)][temp.m-1];
+			}
+			return temp;
+		}
+
 	}
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		System.out.println("현재 날짜를 입력해주세요.");
 		System.out.print("year: "); int y = sc.nextInt();
 		System.out.print("month: "); int m = sc.nextInt();
 		System.out.print("day: "); int d = sc.nextInt();
 		YMD date = new YMD(y, m, d);
 
-		System.out.print("n일 뒤의 날짜를 반환합니다. n= ");
+		System.out.print("n일 뒤의 날짜를 반환합니다. n: ");
 		int n = sc.nextInt();
-		date.after(n);
 		YMD d1 = date.after(n);
 		System.out.printf("%d일 뒤의 날짜는 %d년 %d월 %d일입니다.\n", n, d1.y, d1.m, d1.d);
+
+		System.out.print("k일 앞의 날짜를 반환합니다. k: ");
+		int k = sc.nextInt();
+		YMD d2 = date.before(k);
+		System.out.printf("%d일 앞의 날짜는 %d년 %d월 %d일입니다.\n", k, d2.y, d2.m, d2.d);
 
 	}
 }
