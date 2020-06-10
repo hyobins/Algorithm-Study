67f3543e7313ef6139a02079c854121592327719
Casey Lee <dawnrice6299@gmail.com>
2020-06-11 02:57:26 +0900

    for문 메소드 추가

--- DataStructure/practice3-1.java
+++ DataStructure/practice3-1.java
@@ -2,17 +2,26 @@ package test;
 import java.util.Scanner;
 //선형검색(linear search)
 class test{
-	//요솟수가 n인 배열 a에서 key와 같은 요소를 선형검색
-	static int seqSearch(int[] a, int n, int key) {
-		int i=0;
+	//요솟수가 n인 배열 a에서 key와 같은 요소를 선형검색\
+	//while문 version
+//	static int seqSearch(int[] a, int n, int key) {
+//		int i=0;
+//
+//		while(true) {
+//			if(i==n)
+//				return -1; //검색 실패
+//			if(a[i]==key)
+//				return i; //검색 성공!(인덱스 i 반환)
+//			i++;
+//		}
+//	}
 
-		while(true) {
-			if(i==n)
-				return -1; //검색 실패
-			if(a[i]==key)
-				return i; //검색 성공!(인덱스 i 반환)
-			i++;
-		}
+	//for문 version
+	static int seqSearch(int[] a, int n, int key) {
+		for(int i=0; i<n; i++)
+			if(a[i] == key)
+				return i;
+		return -1;
 	}
 
 	public static void main(String[] args) {
