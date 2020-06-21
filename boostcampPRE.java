95aace9de926714d84b6b8073fab39c21f183a06
Casey Lee <dawnrice6299@gmail.com>
2020-06-21 19:34:23 +0900

    자가진단 6번

---
+++ boostcampPRE.java
@@ -0,0 +1,20 @@
+package test;
+
+class test {
+	static int countOf(int[] arr, int value) {
+		int count =0;
+		for(int i=0;i<arr.length;i++) {
+			if(arr[i] == value)
+				count++;
+		}
+		return count;
+	}
+
+	static int solution(int[] arr) {
+
+	}
+
+	public static void main(String[] args) {
+
+	}
+}
